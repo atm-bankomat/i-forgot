791a3a189e5f9272ed6bb6ef0b089d04edb8e8bf
Christian Dupuis <cd@atomist.com>
2017-09-20 10:40:56 +0200

    support for cf env service

--- src/atomist.config.ts
+++ src/atomist.config.ts
@@ -1,10 +1,16 @@
 import { Configuration } from "@atomist/automation-client/configuration";
+import * as cfenv from "cfenv";
 
 import { HelloWorld } from "./commands/simple/HelloWorld";
 import { CommentOnIssue } from "./events/CommentOnIssue";
 import { NotifyOnPush } from "./events/NotifyOnPush";
 
 const pj = require("../../package.json");
+
+const appEnv = cfenv.getAppEnv();
+const credService = appEnv.getServiceCreds("github-token");
+
+const token = credService ? credService.token : process.env.GITHUB_TOKEN;
 
 export const configuration: Configuration = {
     name: pj.name,
@@ -17,5 +23,5 @@ export const configuration: Configuration = {
         () => new CommentOnIssue(),
         () => new NotifyOnPush(),
     ],
-    token: process.env.GITHUB_TOKEN,
+    token,
 };
