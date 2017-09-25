a06abdfec254b41029e9b2a4d74442f98d76d910
Jessica Kerr <jessitron@gmail.com>
2017-09-25 13:16:57 -0500

    use status instead

--- src/lint-fix/DistillFailedBuild.ts
+++ src/lint-fix/DistillFailedBuild.ts
@@ -11,6 +11,25 @@ import { logger } from "@atomist/automation-client/internal/util/logger";
 import { PushWithRepoSubscription } from "../schema";
 import { teamStream } from "../action-board/helpers";
 
+const byStatus = `
+query($buildStatus: StatusState) {
+  Status(state: $buildStatus, orderBy: id_desc) {
+    targetUrl
+    context
+    commit {
+      author {
+        login
+        person {
+          forename
+          chatId {
+            screenName
+          }
+        }
+      }
+    }
+  }
+}
+`
 const subscription = `
 subscription PushWithRepo {
   Build(provider: "travis") {
@@ -38,31 +57,32 @@ subscription PushWithRepo {
 `
 
 @EventHandler("Provide the end of the log on failed build",
-    subscription)
+    byStatus)
 @Tags("travis")
 export class FailedBuildLog implements HandleEvent<any> {
 
     public handle(e: EventFired<any>, ctx: HandlerContext): Promise<any> {
         logger.info(`Incoming event is %s`, JSON.stringify(e.data, null, 2));
 
-        const buildData = e.data;
+        const statusData = e.data;
         const author: { screenName: string } | string = // string means error
-            e.data.commit ? "No commit on build" :
-                e.data.commit[0].author ? "No author on commit" :
-                    e.data.commit[0].author.person ? "No person on author, GH login " + e.data.commit[0].author :
-                        e.data.commit[0].author.person.chatId ? "No chat ID on person " + e.data.commit[0].author :
-                            e.data.commit[0].author.person.chatId.screenName ?
-                                "No screen name on chatId for person " + + e.data.commit[0].author :
-                                { screenName: e.data.commit[0].author.person.chatId.screenName }
+            statusData.commit ? "No commit on status" :
+                statusData.commit[0].author ? "No author on commit" :
+                    statusData.commit[0].author.person ? "No person on author, GH login " + statusData.commit[0].author :
+                        statusData.commit[0].author.person.chatId ? "No chat ID on person " + statusData.commit[0].author :
+                            statusData.commit[0].author.person.chatId.screenName ?
+                                "No screen name on chatId for person " + + statusData.commit[0].author :
+                                { screenName: statusData.commit[0].author.person.chatId.screenName }
 
         if (typeof author === "string") {
             ctx.messageClient.addressChannels(
-                `There was a failed build here: ${buildData.buildUrl} but I couldn't figure out whose it is. ${author}`,
+                `There was a failed build here: ${statusData.targetUrl} but I couldn't figure out whose it is. ${author}`,
                 teamStream)
             return Promise.resolve({ code: 1 });
         }
 
-        ctx.messageClient.addressUsers("I saw your failed build at " + buildData.buildUrl,
+        ctx.messageClient.addressUsers(
+            `I saw your failed build at ${statusData.targetUrl} with context ${statusData.context} `,
             author.screenName)
         return Promise.resolve({ code: 0 });
     }
