8e123c69ac5d4d26d5c6ca4448d99ef7c656efc5
Jessica Kerr <jessitron@gmail.com>
2017-09-24 21:38:06 -0500

    it does a thing

--- src/atomist.config.ts
+++ src/atomist.config.ts
@@ -6,25 +6,28 @@ import * as cfenv from "cfenv";
 import { UpdateActionBoardsOnIssue } from "./action-board/UpdateActionBoardsOnIssue";
 import { Unassign } from "./action-board/Unassign";
 import { CloseIssue } from "./action-board/Complete";
+import { BuildLog } from "./lint-fix/BuildLog";
 
 const pj = require("../../package.json");
 
 const token = process.env["GITHUB_TOKEN"];
 
 export const configuration: Configuration = {
-    name: "action-board",
-    version: "0.2.1",
+    name: "automate-lint-fix",
+    version: "0.2.3",
     teamId: "T6MFSUPDL",
     commands: [
-        () => new ActionBoard(),
-        () => new ActionBoardUpdate(),
-        () => new CommenceWork(),
-        () => new PostponeWork(),
-        () => new Unassign(),
-        () => new CloseIssue(),
+        // () => new ActionBoard(),
+        // () => new ActionBoardUpdate(),
+        // () => new CommenceWork(),
+        // () => new PostponeWork(),
+        // () => new Unassign(),
+        // () => new CloseIssue(),
+        // build
+        () => new BuildLog()
     ],
     events: [
-        () => new UpdateActionBoardsOnIssue(),
+        //  () => new UpdateActionBoardsOnIssue(),
     ],
     token,
     http: {
